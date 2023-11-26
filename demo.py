3506eea319a32a318abaac4322dd5ad91199c025
204313508 <hht2749655593@vip.qq.com>
2023-11-26 22:55:27 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,32 @@
+import json
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] ="0"
+def loadmodel(modelpath):
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+    from transformers.generation import GenerationConfig
+    from peft import AutoPeftModelForCausalLM
+    tokenizer = AutoTokenizer.from_pretrained(modelpath, trust_remote_code=True)
+    # model = AutoModelForCausalLM.from_pretrained(modelpath, device_map="auto", trust_remote_code=True,use_flash_attn=False).eval()
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        "./checkpoint/checkpoint_0.4k", # path to the output directory
+        device_map="auto",
+        bf16=True,
+        trust_remote_code=True,
+        use_flash_attn=False
+        ).eval()
+    model.generation_config = GenerationConfig.from_pretrained(modelpath, trust_remote_code=True) # 可指定不同的生成长度、top_p等相关超参
+
+    return tokenizer,model
+def llmchat(tokenizer,model,query,historys):
+    response, history = model.chat(tokenizer, query, history = historys)
+    return response,history
+
+tokenizer,model = loadmodel("./model/QWen-7B-Chat")
+while True:
+    print("请输入技能效果：")
+    prompt = input()
+    prompt = "请帮我编写一个技能，技能效果如下：" + prompt
+    history = []
+    llmanswer,history = llmchat(tokenizer,model,prompt,history)
+    print(llmanswer)
+
