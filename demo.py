43e3d48e959c8733b5a64961d61ab13a0d393436
204313508 <hht2749655593@vip.qq.com>
2023-12-12 00:38:17 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,32 +1,15 @@
-import json
-import os
-# os.environ["CUDA_VISIBLE_DEVICES"] ="0"
-def loadmodel(modelpath):
-    from transformers import AutoModelForCausalLM, AutoTokenizer
-    from transformers.generation import GenerationConfig
-    from peft import AutoPeftModelForCausalLM
-    tokenizer = AutoTokenizer.from_pretrained(modelpath, trust_remote_code=True)
-    # model = AutoModelForCausalLM.from_pretrained(modelpath, device_map="auto", trust_remote_code=True,use_flash_attn=False).eval()
-    model = AutoPeftModelForCausalLM.from_pretrained(
-        "./checkpoint/正式版v1.0", # path to the output directory
-        device_map="auto",
-        bf16=True,
-        trust_remote_code=True,
-        use_flash_attn=False
-        ).eval()
-    model.generation_config = GenerationConfig.from_pretrained(modelpath, trust_remote_code=True) # 可指定不同的生成长度、top_p等相关超参
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation import GenerationConfig
+tokenizer = AutoTokenizer.from_pretrained("huskyhong/noname-ai-v2", trust_remote_code=True)
+# model = AutoModelForCausalLM.from_pretrained("huskyhong/noname-ai-v2", device_map="auto", trust_remote_code=True).eval() # 采用gpu加载模型
+model = AutoModelForCausalLM.from_pretrained("huskyhong/noname-ai-v2", device_map="cpu", trust_remote_code=True).eval() # 采用cpu加载模型
+model.generation_config = GenerationConfig.from_pretrained("huskyhong/noname-ai-v2", trust_remote_code=True) # 可指定不同的生成长度、top_p等相关超参
+# 第一代模型请将huskyhong/noname-ai-v2改为huskyhong/noname-ai-v1，轻量版模型请将huskyhong/noname-ai-v2改为huskyhong/noname-ai-v2-light
 
-    return tokenizer,model
-def llmchat(tokenizer,model,query,historys):
-    response, history = model.chat(tokenizer, query, history = historys)
-    return response,history
+prompt = "请帮我编写一个技能，技能效果如下：" + input("请输入技能效果：")
+response, history = model.chat(tokenizer, prompt, history = [])
+print(response)
 
-tokenizer,model = loadmodel("./models/QWen-7B-Chat")
-while True:
-    print("请输入技能效果：")
-    prompt = input()
-    prompt = "请帮我编写一个技能，技能效果如下：" + prompt
-    history = []
-    llmanswer,history = llmchat(tokenizer,model,prompt,history)
-    print(llmanswer)
-
+prompt = "请帮我编写一张卡牌，卡牌效果如下：：" + input("请输入卡牌效果：")
+response, history = model.chat(tokenizer, prompt, history = [])
+print(response)
